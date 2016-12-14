dd37be806d0e3898ec2cf4ee341732052fa6d2a5
sxmaps <sxmaps@sxmapsdemac-mini-2.local>
2016-12-14 16:22:39 +0800

    Initial Commit

---
+++ SJCombox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJCombox
+//
+//  Created by sxmaps on 2016/12/14.
+//  Copyright © 2016年 sxmaps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
